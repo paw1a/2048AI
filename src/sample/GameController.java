c11758a51fce6099086db9f47c0def7dababdd78
Paul Shpakovsky <paw1a@yandex.ru>
2020-10-03 16:51:26 +0300

    Add .jar file

--- src/sample/GameController.java
+++ src/sample/GameController.java
@@ -5,7 +5,6 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
